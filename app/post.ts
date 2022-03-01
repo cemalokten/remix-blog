b0b08af1efd2cffc2fff0e3f75ea44cedab000e9
cemalokten <cemalokten@gmail.com>
2022-03-01 19:37:25 +0000

    Add markdown reading for post

--- app/post.ts
+++ app/post.ts
@@ -7,19 +7,27 @@ import { marked } from 'marked'
 export type Post = {
   slug: string
   title: string
+  tags: string
+  date: string
+  background: Array<string>
 }
 
 export type PostMarkdownAttributes = {
   title: string
+  tags: Array<string>
+  date: string
+  background: Array<string>
 }
 
 const pathOfPosts = path.join(__dirname, '..', 'posts')
 
 const hasValidPostAttributes = (
   attributes: any
-): attributes is PostMarkdownAttributes => {
-  return attributes?.title
-}
+): attributes is PostMarkdownAttributes =>
+  attributes?.title ||
+  attributes?.tags ||
+  attributes?.date ||
+  attributes?.background
 
 export const getPostsTitle = async () => {
   const postDir = await fs.readdir(pathOfPosts)
@@ -29,20 +37,25 @@ export const getPostsTitle = async () => {
       const { attributes }: { attributes: string } = parseFrontMatter(
         file.toString()
       )
+
       invariant(
         hasValidPostAttributes(attributes),
         `${filename} has incorrect title metadata`
       )
+
       return {
         slug: filename.replace(/\.md$/, ''),
         title: attributes.title,
+        tags: attributes.tags,
+        date: attributes.date,
+        background: attributes.background,
       }
     })
   )
 }
 
 export const getPost = async (slug: string) => {
-  const filepath = path.join(pathOfPosts, slug + '.md')
+  const filepath = path.join(pathOfPosts, `${slug}.md`)
   const file = await fs.readFile(filepath)
   const { attributes, body } = parseFrontMatter(file.toString())
   invariant(
@@ -50,5 +63,12 @@ export const getPost = async (slug: string) => {
     `Post ${filepath} is missing valid attributes`
   )
   const html = marked(body)
-  return { slug, html, title: attributes.title }
+  return {
+    slug,
+    html,
+    title: attributes.title,
+    tags: attributes.tags,
+    date: attributes.date,
+    background: attributes.background,
+  }
 }
